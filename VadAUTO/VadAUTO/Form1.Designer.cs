6f20ac35ad7a8754c5222a7a6dfbac36a9d17fc6
CepoiuBogdan <cepoiubogdan@yahoo.com>
2018-12-24 17:49:48 +0200

    client soft

---
+++ VadAUTO/VadAUTO/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace VadAUTO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.parolaBox = new System.Windows.Forms.TextBox();
+            this.conectareButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(509, 159);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(189, 20);
+            this.userBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(453, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(437, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parola";
+            // 
+            // parolaBox
+            // 
+            this.parolaBox.Location = new System.Drawing.Point(509, 205);
+            this.parolaBox.Name = "parolaBox";
+            this.parolaBox.Size = new System.Drawing.Size(189, 20);
+            this.parolaBox.TabIndex = 3;
+            // 
+            // conectareButton
+            // 
+            this.conectareButton.FlatAppearance.BorderSize = 0;
+            this.conectareButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.conectareButton.Font = new System.Drawing.Font("Malgun Gothic", 14.25F);
+            this.conectareButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.conectareButton.Location = new System.Drawing.Point(509, 258);
+            this.conectareButton.Name = "conectareButton";
+            this.conectareButton.Size = new System.Drawing.Size(189, 39);
+            this.conectareButton.TabIndex = 4;
+            this.conectareButton.Text = "Conectare";
+            this.conectareButton.UseVisualStyleBackColor = true;
+            this.conectareButton.Click += new System.EventHandler(this.conectareButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.closeButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.closeButton.Location = new System.Drawing.Point(756, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(32, 23);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.conectareButton);
+            this.Controls.Add(this.parolaBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox parolaBox;
+        private System.Windows.Forms.Button conectareButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ VadAUTO/VadAUTO/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace VadAUTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void conectareButton_Click(object sender, EventArgs e)
+        {
+            LogIn client = new LogIn();
+            client.command = "Login";
+            client.user = userBox.Text;
+            client.pass = parolaBox.Text;
+
+            string json = JsonConvert.SerializeObject(client);
+           
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+    class LogIn
+        {
+            public string command;
+            public string user;
+            public string pass;
+        }
+}
